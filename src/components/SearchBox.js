02fe77c0f48f877a6c5cffc3a1b3148d3ef650e9
Thakrisp <ryancrispin@gmail.com>
2020-06-08 19:46:39 -0400

    updates to improve PWA performance

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,17 @@
 import React from "react";
 
-const SearchBox = ({searchfield, searchChange}) => {
+const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className="pa2">
-        <input 
-          className="pa3 ba b--green bg-lightest-blue" 
-          type="search" 
-          placeholder="search for a robo friend"
-          onChange={searchChange}
-        />
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search for a robo friend"
+        onChange={searchChange}
+        aria-label="Search robots"
+      />
     </div>
-    );
+  );
 };
 
 export default SearchBox;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createLogger } from "redux-logger";
-import thunkMiddleWare from 'redux-thunk';
+import thunkMiddleWare from "redux-thunk";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import "./index.css";
 import App from "../src/containers/App";
@@ -12,8 +12,11 @@ import { searchRobots, requestRobots } from "./reducers";
 
 const logger = createLogger();
 
-const rootReducer = combineReducers({searchRobots, requestRobots})
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleWare, logger));
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleWare, logger)
+);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -27,4 +30,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
